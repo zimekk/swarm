1a8558b122399ec2547e95ecb5b89b6418e2c8a1
Marcin Makarewicz <marcin@makarewicz.eu>
2020-09-22 01:25:45 +0200

    fix: correct use elastic-apm-node in app

--- packages/ssr/src/index.js
+++ packages/ssr/src/index.js
@@ -8,7 +8,20 @@ import { name, version } from "../package";
 export default render;
 
 if (process.env.NODE_ENV === "production") {
-  const apm = require("elastic-apm-node").start();
+  // Add this to the VERY top of the first file loaded in your app
+  const apm = require("elastic-apm-node").start({
+    // Override service name from package.json
+    // Allowed characters: a-z, A-Z, 0-9, -, _, and space
+    serviceName: "app",
+
+    // Use if APM Server requires a token
+    secretToken:
+      "xxVpmQB2HMzCL9PgBHVrnxjNXXw5J7bd79DFm6sjBJR5HPXDhcF8MSb3vv4bpg44",
+
+    // Set custom APM Server URL (default: http://localhost:8200)
+    serverUrl: "http://apm-server:8200",
+  });
+
   const express = require("express");
 
   const server = express()
